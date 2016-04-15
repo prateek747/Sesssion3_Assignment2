bd326e7e86a28d47ab44ce22e84ef8aba6400eef
prateek747 <prateek.mahesh747@gmail.com>
2016-04-15 06:56:51 +0530

    Added files via upload

---
+++ pat.java
@@ -0,0 +1,37 @@
+package patern_session3;
+
+public class pat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ck = 0,c = 2;
+		while(c>0){
+			if (ck == 0){
+				for (int i = 1; i<=5; i++){
+					for(int j = 1;j<=i;j++){
+				System.out.print(j);
+				
+			    }
+					System.out.println();
+					ck ++;
+			}
+		}
+			else{
+				for(int i=1,r=4;i<=4;i++,r--){
+					for(int j = 1; j<=r; j++){
+						System.out.print(j);
+						
+				}
+					System.out.println();
+					
+			}
+				
+					
+				}c--;
+			}
+            
+        }
+           
+	}
+
+
